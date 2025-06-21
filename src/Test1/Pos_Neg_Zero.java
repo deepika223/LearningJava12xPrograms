94561f6d0042564f42b0d69840a6631a432a08f2
deepika223 <deepikalk97@gmail.com>
2025-06-21 12:37:16 +0530

    Operators

---
+++ src/Test1/Pos_Neg_Zero.java
@@ -0,0 +1,24 @@
+package Test1;
+
+public class Pos_Neg_Zero {
+    public static void main(String[] args) {
+
+        int num=-15;
+
+        if(num>0)
+        {
+            System.out.println("Positive");
+        }
+
+        else if(num<0)
+        {
+            System.out.println("Negative");
+        }
+
+        else
+        {
+            System.out.println("Zero");
+        }
+
+    }
+}
