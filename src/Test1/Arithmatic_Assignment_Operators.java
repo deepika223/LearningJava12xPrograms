678accbd82116350aa7a490780209e1b5d6ec9ee
deepika223 <deepikalk97@gmail.com>
2025-06-21 12:13:23 +0530

    Operators

---
+++ src/Test1/Arithmatic_Assignment_Operators.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class Arithmatic_Assignment_Operators {
+    public static void main(String[] args) {
+
+        int a=10,b=5;
+
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Substraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulus:"+(a%b));
+    }
+}
